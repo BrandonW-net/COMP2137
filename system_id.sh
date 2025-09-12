bda553b479e140c623ae1fd72beef2b5bad158be
Brandon Williams <169378108+BrandonW-net@users.noreply.github.com>
2025-09-12 08:58:29 -0400

    Added practice scripts for: hello world, system update, system info and system status

---
+++ system_id.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script is to display the current system information
+
+# Display the hostname
+echo -n "Hostname: " 
+ hostname
+
+# Display ip address
+echo -n "IP address: "
+ hostname -I
+
+# Display system status
+echo -n "System Runtime: "
+ uptime
+
+
